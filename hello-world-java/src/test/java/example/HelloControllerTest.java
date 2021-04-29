b57d93f1178fd4412927a23f26aa530ac50b1529
Stephane MICHELOUD <stephane.micheloud@gmail.com>
2021-04-29 18:34:45 +0200

    updated mn version (1.3.4->2.4.2)

--- hello-world-java/src/test/java/example/HelloControllerTest.java
+++ hello-world-java/src/test/java/example/HelloControllerTest.java
@@ -2,7 +2,8 @@ package example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import io.micronaut.test.annotation.MicronautTest;
+//import io.micronaut.test.annotation.MicronautTest; // deprecated since 1.3
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
